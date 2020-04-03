4ca533f5a1ce89b65712d7291543685d79727827
Nick Magidson <Nickmagi24@gmail.com>
2020-04-03 16:28:42 -0400

    Added front-end

---
+++ js/script.js
@@ -0,0 +1,64 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [
+  {
+      quote: 'placholder',
+      source: 'placeholder',
+      citation: 'placeholder', // optional
+      year: 'placeholder' // optional
+  },
+  {
+      quote: 'placholder',
+      source: 'placeholder',
+      citation: 'placeholder', // optional
+      year: 'placeholder' // optional
+  },
+  {
+      quote: 'placholder',
+      source: 'placeholder',
+      citation: 'placeholder', // optional
+      year: 'placeholder' // optional
+  },
+  {
+      quote: 'placholder',
+      source: 'placeholder',
+      citation: 'placeholder', // optional
+      year: 'placeholder' // optional
+  },
+  {
+      quote: 'placholder',
+      source: 'placeholder',
+      citation: 'placeholder', // optional
+      year: 'placeholder' // optional
+  }
+]
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
