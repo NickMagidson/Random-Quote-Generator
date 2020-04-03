3b8c2e4dfcc401f3c8032535795bbc83ea729f45
Nick Magidson <Nickmagi24@gmail.com>
2020-04-03 15:38:57 -0400

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,32 @@
+let quotes = [
+    {
+        quote: 'placholder',
+        source: 'placeholder',
+        citation: 'placeholder', // optional
+        year: 'placeholder' // optional
+    },
+    {
+        quote: 'placholder',
+        source: 'placeholder',
+        citation: 'placeholder', // optional
+        year: 'placeholder' // optional
+    },
+    {
+        quote: 'placholder',
+        source: 'placeholder',
+        citation: 'placeholder', // optional
+        year: 'placeholder' // optional
+    },
+    {
+        quote: 'placholder',
+        source: 'placeholder',
+        citation: 'placeholder', // optional
+        year: 'placeholder' // optional
+    },
+    {
+        quote: 'placholder',
+        source: 'placeholder',
+        citation: 'placeholder', // optional
+        year: 'placeholder' // optional
+    }
+]
